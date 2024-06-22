c645f2e3ca2ab9d378221335404e44f6964d98ec
OkanoKenshin <prometheus122a@gmail.com>
2024-06-22 10:56:50 +0900

    重複を削除

--- script_folder/コード.js
+++
@@ -1,9 +0,0 @@
-let linkingUltAndUnit = new LinkingUltAndUnit();
-function onEdit(e) {
-  let editPoint = e.range;
-  return editPoint;
-}
-
-editedCell = linkingUltAndUnit.selectedParent(onEdit);
-editCellRow = linkingUltAndUnit.searchParent(editedCell);
-linkingUltAndUnit.setPriority(editCellRow);
